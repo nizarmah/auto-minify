24cb9851fe829777c1073781ef014a0c7b0336b0
Nizar <nizarmah@hotmail.com>
2024-03-17 21:59:13 +0400

    Add maxdepth input test (#35)

---
+++ tests/maxdepth/main.js
@@ -0,0 +1,7 @@
+function print_hello() {
+	console.log('hello world from maxdepth');
+}
+
+window.addEventListener('load', function () {
+	print_hello();
+});
---
+++ tests/maxdepth/not-affected/main.js
@@ -0,0 +1,7 @@
+function print_hello() {
+	console.log('hello world from maxdepth/not-affected');
+}
+
+window.addEventListener('load', function () {
+	print_hello();
+});
