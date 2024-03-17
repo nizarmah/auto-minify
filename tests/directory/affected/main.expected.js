d056427f1078707d5353af79cca274188a47a79f
Nizar <nizarmah@hotmail.com>
2024-03-17 20:19:04 +0400

    Fix test expected output

--- tests/directory/affected/main.expected.js
+++ tests/directory/affected/main.expected.js
@@ -1,1 +1,1 @@
-function print_hello(){console.log("hello world from directory/targeted")}window.addEventListener("load",function(){print_hello()});
+function print_hello(){console.log("hello world from directory/affected")}window.addEventListener("load",function(){print_hello()});
